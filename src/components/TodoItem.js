2d724cd9eccc948ec1a596ca1f1bb83d59f1e9a9
Mauricio Laine <laine.mau@gmail.com>
2019-06-25 08:24:46 -0700

    set a state in the class itself, and change the background of the item based on the state

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,15 +1,33 @@
 
 import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 
 
 export default class TodoItem extends Component {
+  // 1 easy way to define a state, directly in the class itself
+  state = {
+    completed: false
+  }
+  // 2 define the initial state define in incoming props  
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     completed: props.title == '1. Go to the store'
+  //   }
+  // } 
+  
+  toggleTodo = () => {
+    this.setState({completed: !this.state.completed})
+  }
+
   render() {
       console.log("props", this.props)
     return (
-      <View style={styles.container}>
-          <Text style={styles.item}>{this.props.title}</Text>
-      </View>
+      <TouchableOpacity onPress={this.toggleTodo}>
+          <Text style={[styles.item, {
+            backgroundColor: (this.state.completed ? 'grey': 'transparent')
+          }]}>{this.props.title}</Text>
+      </TouchableOpacity>
     );
   }
 }
